044799149329a0352144c4852ce91b6412aa274f
nikita <heksus23@gmail.com>
2016-12-10 14:05:57 +0200

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('unable to connect to MongoDb server');
+    }
+    console.log('Connected to MongoDb server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('584be69cf7bf7eaadae47d69')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result)=>{
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('584bcfa11e965f12a0a3824b')
+    }, {
+        $set: {
+            name: 'Nikita'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
